301bc14feab724cd0670df9462f1a8fcf20bdd0b
yungchi <thomas890201@gmail.com>
2024-03-15 19:13:46 -0400

    initial commit

---
+++ test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+./mdriver-dbg -d 2 -v 2 -f traces/syn-array-short.rep 
+./mdriver-dbg -d 2 -v 2 -f traces/syn-string-short.rep 
+./mdriver-dbg -d 2 -v 2 -f traces/syn-struct-short.rep 
+./mdriver-dbg -d 2 -v 2 -f traces/syn-mix-short.rep 
+./mdriver-dbg -d 2 -v 2 -f traces/ngram-fox1.rep 
+./mdriver-dbg -d 2 -v 2 -f traces/syn-mix-realloc.rep 
